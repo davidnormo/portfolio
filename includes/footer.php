deeb046b9f46363a3eb21c8a08986ecc4d640c1d
David Normington <david.normo@gmail.com>
2013-10-20 18:57:59 +0100

    index page complete

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+		</div>
+	</section>
+	<div class="push"></div>
+	</div>
+	<section id="footer">
+		<div class="container">
+			<p>&copy; 2013 davidnormo.com</p>
+		</div>
+	</section>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<? include 'includes/header.php' ?>
+			<h1>Hello there</h1>
+			<p>Thanks for visiting my portfolio. This is my space to display my work, 
+			write some thoughts down and generally try to keep up with what's going on. 
+			I hope to keep the site updated as best I can, 
+			please shoot me an email if you discover a problem.</p>
+			<p>Thanks - David</p>
+			<hr />
+			<h2>The who and what</h2>
+			<p>I am David Normington, currently studying Computer Science at the University of Kent, UK.
+			I'm a web developer, I mostly work on server side projects but started out learning 
+			front end languages.</p>
+			<p>This is my portfolio and space to publish my work and keep things organised. 
+			My blog also lives here which I use to annotate some of my work but it's a lonely old thing,
+			without a lot of life but useful to me to jot down the odd thought or experiment.</p>
+<? include 'includes/footer.php' ?>
