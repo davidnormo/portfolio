34a591ebb05e4a1149b238b97a63ba3c6986aba8
David Normington <david.normo@gmail.com>
2014-04-30 14:58:41 +0100

    Added new post

--- www/index.php
+++ www/index.php
@@ -5,7 +5,7 @@
 		<!-- Start post #2 -->
 		<div class="post" id="2">
 			<h2>Developing this site</h2>
-			<p>Posted: xx March 2014</p>
+			<p>Posted: 30 April 2014</p>
 			<p>I spent an afternoon putting this site together. As you can tell it is fairly minimal. Most elements aren't even styled. My requirements for the site are as follows:</p>
 <ol><li>I wanted something to get up and running quickly</li>
 <li>Display what content there is clearly.</li>
@@ -47,7 +47,14 @@
 <p>So I downloaded a pre-compiled version of Bootstrap, updated the markup to point towards the local copies of the assets and took a peak into the source. I found that without CSS variables this would take some time and I would probably make some mistakes as I was doing this quickly.</p>
 <p>So I downloaded the LESS source of Bootstrap and proceeded to <code>npm install</code> the project dependancies. 2 minutes later, I found the relevant <code>variables.less</code> file. I made a few changes, which included added the Domine serif font from <a href="http://www.google.com/fonts">Google Fonts</a> and making my colour change. I ran <code>grunt dist</code> and then waited for about 5-8 seconds for that to complete.</p> 
 <p><b>5-8 seconds!</b> That completely shot my workflow. I liked that everything I wanted (more or less) came right out of the box with Bootstrap but I didn't want to wait so long to make some minor CSS changes, which as we all know, turns into a lot of back and forth between the browser and the editor.</p>
-<p>I scrapped Bootstrap but kept the HTML5 boilerplate. At this point I was in two minds whether to write straight CSS or to take the SASS path. I decided on the later as I knew it wouldn't take me long to get setup and would benefit me more in the long run.</p>
+<p>I scrapped Bootstrap but kept the HTML5 boilerplate. At this point I was in two minds whether to write straight CSS or to take the SASS path. I decided on the later as I knew it wouldn't take long to get setup and would benefit me more in the long run.</p>
+<p>I added <a href="http://necolas.github.io/normalize.css/">normalize.css</a> and a few basic styles, a quick "responsive" layout (using a fluid width), and used <a href="http://prismjs.com/">PrismJS</a> for syntax highlighting.</p>
+<p>With a rough first version working, I am now looking into improving the blog experience. Some features I have in mind at the moment include:</p>
+<ul><li>Individual post pages</li>
+<li><a href="http://disqus.com/">Disqus</a> commenting</li>
+<li>Post summaries on the index page</li>
+<li>Writing posts in Markdown</li></ul>
+<p>Of course the starting point would be, what's out there already?<br />The source for this site can be found at this Github repo: <a href="https://github.com/davidnormo/portfolio">https://github.com/davidnormo/portfolio</a></p>
 		</div>
 		<!-- End post #2 -->
 		<hr />
